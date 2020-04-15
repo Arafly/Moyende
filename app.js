6fc2f02113aea2751b0daa0f3613adba1723397f
Ayinde Ayobami <ayinde.ayobami@venturegardengroup.com>
2020-04-15 23:25:02 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const port = 8080
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+app.use((req, res, next)=> {
+    console.log('Testing out this middleware 💪🏾');
+    next();
+})
+
+// Start Server
+app.listen(port, () => {
+    console.log(`Running on the designated port : ${port}`)
+})
